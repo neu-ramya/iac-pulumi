cc540210d5f14c6a8382ec2f46f84e0e6aa616d4
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2023-10-11 23:11:38 -0400

    subnet creation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,59 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-const main = new aws.ec2.Vpc("vpc-csye6225", {
-    cidrBlock: "10.0.0.0/16",
-    instanceTenancy: "default",
-    tags: {
-        Name: "vpc-csye6225",
-    },
-});
+async function main() {
+    const awsRegion = "us-west-2";
+
+    const vpc = new aws.ec2.Vpc("vpc-csye6225", {
+        cidrBlock: "10.0.0.0/16",
+        instanceTenancy: "default",
+        tags: {
+            Name: "vpc-csye6225",
+        },
+    });
+
+    const numberOfAZsToSelect = 3;
+    let  numAZsToCreate;
+
+    const availableAZs = await aws.getAvailabilityZones({
+        state: "available"
+    });
+
+    const numAZs = availableAZs.names.length
+    if(numAZs > numberOfAZsToSelect) {
+        numAZsToCreate = numberOfAZsToSelect
+    } else {
+        numAZsToCreate = numAZs
+    }
+
+    // Choose 3 availability zones from the list.
+    const selectedAZs = availableAZs.names?.slice(0, numberOfAZsToSelect) 
+    
+    
+    for (const az of selectedAZs) {
+        const publicSubnet = new aws.ec2.Subnet(`${az}-public-subnet`, {
+            vpcId: vpc.id,
+            availabilityZone: az,
+            cidrBlock: "10.0.0.0/24", // Replace with the desired CIDR block for the public subnet.
+            mapPublicIpOnLaunch: true, // Enable auto-assign public IP addresses.
+            tags: {
+                Name: "public-subnet",
+            },
+        });
+    
+        const privateSubnet = new aws.ec2.Subnet(`${az}-private-subnet`, {
+            vpcId: vpc.id,
+            availabilityZone: az,
+            cidrBlock: "10.0.1.0/24", // Replace with the desired CIDR block for the private subnet.
+            tags: {
+                Name: "private-subnet",
+            },
+        });
+    }
+
+
+    
+
+}
+
+main();
