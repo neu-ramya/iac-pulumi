cbe12cb63feac8352dfc270125c0b517f2846f79
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2023-10-12 00:11:19 -0400

    add all resrouces
    inital commit for demo stack

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
 async function main() {
-    const awsRegion = "us-west-2";
+    let pulumiConfig = new pulumi.Config("pulumi");
 
-    const vpc = new aws.ec2.Vpc("vpc-csye6225", {
-        cidrBlock: "10.0.0.0/16",
+    const vpc = new aws.ec2.Vpc(pulumiConfig.require("vpcName"), {
+        cidrBlock: pulumiConfig.require("vpcCIDRblock"),
         instanceTenancy: "default",
         tags: {
             Name: "vpc-csye6225",
+            Owner: pulumiConfig.require("ownerTag")
         },
     });
 
@@ -26,10 +27,10 @@ async function main() {
         numAZsToCreate = numAZs
     }
 
-    // Choose 3 availability zones from the list.
     const selectedAZs = availableAZs.names?.slice(0, numAZsToCreate) 
-    
-    
+    let  publicSubnetArray = [];
+    let  privateSubnetArray = [];
+
     for (const az of selectedAZs) {
         const publicSubnet = new aws.ec2.Subnet(`${az}-public-subnet`, {
             vpcId: vpc.id,
@@ -38,22 +39,72 @@ async function main() {
             mapPublicIpOnLaunch: true,
             tags: {
                 Name: "public-subnet",
+                Owner: pulumiConfig.require("ownerTag")
             },
         });
-    
+        
         const privateSubnet = new aws.ec2.Subnet(`${az}-private-subnet`, {
             vpcId: vpc.id,
             availabilityZone: az,
             cidrBlock: "10.0.1.0/24",
             tags: {
                 Name: "private-subnet",
+                Owner: pulumiConfig.require("ownerTag")
             },
+        });
+
+        publicSubnetArray.push(publicSubnet);
+        privateSubnetArray.push(privateSubnet);
+    }
+
+    const internetGateway = new aws.ec2.InternetGateway(pulumiConfig.require("internetGatewayName"), {
+        tags: {
+            Name: "ramya-ig",
+            Owner: pulumiConfig.require("ownerTag")
+        },
+    });
+    
+    const internetGatewayAttachment = new aws.ec2.InternetGatewayAttachment(pulumiConfig.require("internetGatewayAttachmentName"), {
+        vpcId: vpc.id,
+        internetGatewayId: internetGateway.id,
+    });
+    
+
+    const publicRouteTable = new aws.ec2.RouteTable(pulumiConfig.require("publicRouteTable"), {
+        vpcId: vpc.id,
+        tags: {
+            Name: "ramya-public-route-table",
+            Owner: pulumiConfig.require("ownerTag")
+        },
+    });
+    
+    const privateRouteTable = new aws.ec2.RouteTable(pulumiConfig.require("privateRouteTable"), {
+        vpcId: vpc.id,
+        tags: {
+            Name: "private-route-table",
+            Owner: pulumiConfig.require("ownerTag")
+        },
+    });
+
+    for (const pubSubnet of publicSubnetArray) {
+        new aws.ec2.RouteTableAssociation(`public-subnet-association-${Math.floor(Math.random() * 1000) + 1}`, {
+            subnetId: pubSubnet.id,
+            routeTableId: publicRouteTable.id,
         });
     }
 
+    for (const privSubnet of privateSubnetArray) {
+        new aws.ec2.RouteTableAssociation(`private-subnet-association-${Math.floor(Math.random() * 1000) + 1}`, {
+            subnetId: privSubnet.id,
+            routeTableId: privateRouteTable.id,
+        });
+    }
 
-    
-
+    new aws.ec2.Route(pulumiConfig.require("publicRouteName"), {
+        routeTableId: publicRouteTable.id,
+        destinationCidrBlock: pulumiConfig.require("openCIDRblock"),
+        gatewayId: internetGateway.id,
+    });
 }
 
 main();
