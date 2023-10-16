691f34797295d5771355be6540625b33fbae7a8f
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2023-10-16 14:24:34 -0400

    pulumi code with ec2 setup in main

--- src/ec2.ts
+++ src/ec2.ts
@@ -1,21 +1,42 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+const  networking= require("./index");
 
 export default async function ec2() {
     const defaultVpc = await aws.ec2.getVpc({
         default: true,
       });
     
-    const allowTls = new aws.ec2.SecurityGroup("allowTls", {
+    const securityGroup = new aws.ec2.SecurityGroup("application security group", {
         description: "Allow TLS inbound traffic",
         vpcId: defaultVpc.id,
         ingress: [{
+            description: "SSH from VPC",
+            fromPort: 22,
+            toPort: 22,
+            protocol: "tcp",
+            cidrBlocks: ["127.0.0.1/32"],
+        },
+        {
+            description: "HTTP from VPC",
+            fromPort: 80,
+            toPort: 80,
+            protocol: "tcp",
+            cidrBlocks: ["127.0.0.1/32"],
+        },
+        {
             description: "TLS from VPC",
             fromPort: 443,
             toPort: 443,
             protocol: "tcp",
-            // cidrBlocks: [defaultVpc.cidrBlock],
-            // ipv6CidrBlocks: [defaultVpc.ipv6CidrBlock],
+            cidrBlocks: ["127.0.0.1/32"],
+        },
+        {
+            description: "Application port",
+            fromPort: 3000,
+            toPort: 3000,
+            protocol: "tcp",
+            cidrBlocks: ["127.0.0.1/32"],
         }],
         egress: [{
             fromPort: 0,
@@ -25,8 +46,29 @@ export default async function ec2() {
             ipv6CidrBlocks: ["::/0"],
         }],
         tags: {
-            Name: "allow_tls",
+            Name: "application security group",
         },
     });
+
+    // const ami = pulumi.output(aws.ec2.getAmi({
+    //     mostRecent: true,
+    //     owners: [ "042487768049" ],
+    //     nameRegex: "csye6225-debian12*",
+    // }));
+    
+    // // Create and launch an Amazon Linux EC2 instance into the public subnet.
+    // const instance = new aws.ec2.Instance("instance", {
+    //     ami: ami.id,
+    //     instanceType: "t3.nano",
+    //     subnetId: networking. subnet.id,
+    //     vpcSecurityGroupIds: [
+    //         securityGroup.id,
+    //     ],
+    // });
+    
+    // // Export the instance's publicly accessible URL.
+    // module.exports = {
+    //     instanceURL: pulumi.interpolate `http://${instance.publicIp}`,
+    // };
 
 }
