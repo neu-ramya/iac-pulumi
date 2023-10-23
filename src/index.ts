b80fbd6aa34e526d7adc78e91d61be8975ef6cb9
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2023-10-22 22:40:33 -0400

    refactored a lot

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@ import * as pulumi from "@pulumi/pulumi";
 import * as ec2 from "./ec2";
 import * as networking from "./networking";
 import * as rds from "./rds";
-import { Instance } from "@pulumi/aws/rds/instance";
 let pulumiConfig = new pulumi.Config("pulumi");
 
 async function main() {
   let vpcCidr = pulumiConfig.require("vpcCIDRblock");
-  let pubRouteName = pulumiConfig.require("publicRouteName")
+  let pubRouteName = pulumiConfig.require("publicRouteName");
   let vpcName = pulumiConfig.require("vpcName");
   let ec2Name = pulumiConfig.require("ec2Name");
   let igName = pulumiConfig.require("internetGatewayName");
@@ -16,6 +15,13 @@ async function main() {
   let pubRTName = pulumiConfig.require("publicRouteTableName");
   let privRTName = pulumiConfig.require("privateRouteTableName");
   let sgName = pulumiConfig.require("sgName");
+
+  let sshPort = pulumiConfig.require("SSHport");
+  let appPort = pulumiConfig.require("Appport");
+  let httpPort = pulumiConfig.require("HTTPport");
+  let httpsPort = pulumiConfig.require("HTTPSport");
+  let openCIDRblock = pulumiConfig.require("openCIDRblock");
+  let allPort = pulumiConfig.require("allPort");
 
   let AMIShareUsers = pulumiConfig.require("AMIShareUsers");
   let AMIFilterRegex = pulumiConfig.require("AMISelectorRegex");
@@ -30,26 +36,106 @@ async function main() {
   let subnet = await networking.subnets(vpc);
   let ig = await networking.internetGateway(igName, igAttachmentName, vpc);
   networking.routing(pubRTName, privRTName, pubRouteName, vpc, ig);
-  
+
   let ami = await ec2.ami([AMIShareUsers], AMIFilterRegex);
+
+  let ec2SecurityGroup = await ec2.emptySecurityGroup(vpc, sgName);
+  await ec2.addCIDRSecurityGroupRule(
+    "SSH Port",
+    "tcp",
+    ec2SecurityGroup.id,
+    sshPort,
+    sshPort,
+    "ingress",
+    ipAddressAsString
+  );
+  await ec2.addCIDRSecurityGroupRule(
+    "HTTP Port",
+    "tcp",
+    ec2SecurityGroup.id,
+    httpPort,
+    httpPort,
+    "ingress",
+    ipAddressAsString
+  );
+  await ec2.addCIDRSecurityGroupRule(
+    "HTTPS Port",
+    "tcp",
+    ec2SecurityGroup.id,
+    httpsPort,
+    httpsPort,
+    "ingress",
+    ipAddressAsString
+  );
+  await ec2.addCIDRSecurityGroupRule(
+    "Application Port",
+    "tcp",
+    ec2SecurityGroup.id,
+    appPort,
+    appPort,
+    "ingress",
+    ipAddressAsString
+  );
+  await ec2.addCIDRSecurityGroupRule(
+    "Outbound",
+    "-1",
+    ec2SecurityGroup.id,
+    allPort,
+    allPort,
+    "egress",
+    openCIDRblock
+  );
 
   let rdspg = await rds.createRDSparametergroup();
   let rdssubnet = await rds.createSubnetGroup(subnet[1]);
-  let ec2SecurityGroup = await ec2.emptySecurityGroup(vpc, sgName);
-  let rdsSecurityGroup = await ec2.emptySecurityGroup(vpc, "EC2 security group");
-  await ec2.addSecurityGroupRule("EC2 security group rule", "tcp", rdsSecurityGroup.id, ec2SecurityGroup.id, 3306, 3306, "ingress");
-  // await ec2.addCIDRSecurityGroupRule("Outbound", "-1",rdsSecurityGroup.id, 0, 0, "egress", "0.0.0.0/0")
+  let rdsSecurityGroup = await ec2.emptySecurityGroup(
+    vpc,
+    pulumiConfig.require("rdsSecurityGroupName")
+  );
+  
+  await ec2.addSecurityGroupRule(
+    pulumiConfig.require("rdsSecurityGroupName"),
+    "-1",
+    rdsSecurityGroup.id,
+    ec2SecurityGroup.id,
+    allPort,
+    allPort,
+    "ingress"
+  );
+  await ec2.addCIDRSecurityGroupRule(
+    "All local port for RDS",
+    "-1",
+    rdsSecurityGroup.id,
+    allPort,
+    allPort,
+    "ingress",
+    ipAddressAsString
+  );
+  await ec2.addCIDRSecurityGroupRule(
+    "Outbound-rds",
+    "-1",
+    rdsSecurityGroup.id,
+    allPort,
+    allPort,
+    "egress",
+    openCIDRblock
+  );
+  let rdsinstance = await rds.createRDSinstance(
+    rdssubnet,
+    rdspg,
+    rdsSecurityGroup.id
+  );
 
-  let rdsinstance = await rds.createRDSinstance(rdssubnet, rdspg, rdsSecurityGroup.id);
-  let env = await ec2.createEnvFile(rdsinstance, "/home/admin/target/dbenv");
-
-  await ec2.addCIDRSecurityGroupRule("SSH Port", "tcp", ec2SecurityGroup.id, 22, 22, "ingress", ipAddressAsString)
-  await ec2.addCIDRSecurityGroupRule("HTTP Port", "tcp", ec2SecurityGroup.id, 80, 80, "ingress", ipAddressAsString)
-  await ec2.addCIDRSecurityGroupRule("HTTPS Port", "tcp",ec2SecurityGroup.id, 443, 443, "ingress", ipAddressAsString)
-  await ec2.addCIDRSecurityGroupRule("Application Port", "tcp",ec2SecurityGroup.id, 3000, 3000, "ingress", ipAddressAsString)
-  await ec2.addCIDRSecurityGroupRule("Outbound", "-1",ec2SecurityGroup.id, 0, 0, "egress", "0.0.0.0/0")
-
-  let instance = await ec2.ec2Instance(ec2Name, ami.id, ec2SecurityGroup.id, subnet[0][0], env);
+  pulumi.all([rdsinstance.address]).apply(async ([serverName]) => {
+    let env = await ec2.createEnvFile(serverName, "/opt/csye6225/.env");
+    await ec2.ec2Instance(
+      ec2Name,
+      ami.id,
+      ec2SecurityGroup.id,
+      subnet[0][0],
+      env
+    );
+  });
 }
 
 main();
