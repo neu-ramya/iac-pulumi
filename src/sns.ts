a28692be2b2dd1c0ed6019bbf27aa9a532f7c04f
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2023-11-24 20:19:34 -0500

    working dynamo

--- src/sns.ts
+++ src/sns.ts
@@ -1,12 +1,22 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-export async function createSnsTopic(){
-const csyeSnsTopic = new aws.sns.Topic("csyeSnsTopic", {});
-const snsTopicPolicy = csyeSnsTopic.arn.apply(arn => aws.iam.getPolicyDocumentOutput({
-    policyId: "__default_policy_ID",
-    statements: [{
-        actions: [
+export async function createSnsSubscription(topicARN: any, endPointARN: any){
+    const userUpdatesSqsTarget = new aws.sns.TopicSubscription("csye-topic-subscription", {
+        endpoint: endPointARN,
+        protocol: "lambda",
+        topic: topicARN,
+    });
+}
+
+export async function createSnsTopic() {
+  const csyeSnsTopic = new aws.sns.Topic("csye-sns-topic", {});
+  const snsTopicPolicy = csyeSnsTopic.arn.apply((arn) =>
+    aws.iam.getPolicyDocumentOutput({
+      policyId: "__default_policy_ID",
+      statements: [
+        {
+          actions: [
             "SNS:Subscribe",
             "SNS:SetTopicAttributes",
             "SNS:RemovePermission",
@@ -16,29 +26,35 @@ const snsTopicPolicy = csyeSnsTopic.arn.apply(arn => aws.iam.getPolicyDocumentOu
             "SNS:GetTopicAttributes",
             "SNS:DeleteTopic",
             "SNS:AddPermission",
-        ],
-        conditions: [{
-            test: "StringEquals",
-            variable: "AWS:SourceOwner",
-            values: ["042487768049"],
-        }],
-        effect: "Allow",
-        principals: [{
-            type: "AWS",
-            identifiers: ["*"],
-        }],
-        resources: [arn],
-        sid: "__default_statement_ID",
-    }],
-}));
-const snsPolicy = new aws.sns.TopicPolicy("snsPolicy", {
+          ],
+          conditions: [
+            {
+              test: "StringEquals",
+              variable: "AWS:SourceOwner",
+              values: ["042487768049"],
+            },
+          ],
+          effect: "Allow",
+          principals: [
+            {
+              type: "AWS",
+              identifiers: ["*"],
+            },
+          ],
+          resources: [arn],
+          sid: "__default_statement_ID",
+        },
+      ],
+    })
+  );
+  const snsPolicy = new aws.sns.TopicPolicy("csye-sns-policy", {
     arn: csyeSnsTopic.arn,
-    policy: snsTopicPolicy.apply(snsTopicPolicy => snsTopicPolicy.json),
-});
-return snsPolicy;
+    policy: snsTopicPolicy.apply((snsTopicPolicy) => snsTopicPolicy.json),
+  });
+  return snsPolicy;
 }
 
 module.exports = {
-    createSnsTopic: createSnsTopic,
-  };
-  
+  createSnsTopic: createSnsTopic,
+  createSnsSubscription: createSnsSubscription
+};
